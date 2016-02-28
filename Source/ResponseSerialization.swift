77f89e9a86dad7e1fcecb809b5d57c7a250ed52a
Henrik Wittland <henrik.wittland@hwigmbh.de>
2016-02-28 15:12:29 +0100

    source formatting

--- Source/ResponseSerialization.swift
+++ Source/ResponseSerialization.swift
@@ -135,13 +135,13 @@ extension Request {
                 serializationCompletedTime: CFAbsoluteTimeGetCurrent()
             )
 
-                    let response = Response<T.SerializedObject, T.ErrorObject>(
-                        request: self.request,
-                        response: self.response,
-                        data: self.delegate.data,
+            let response = Response<T.SerializedObject, T.ErrorObject>(
+                request: self.request,
+                response: self.response,
+                data: self.delegate.data,
                 result: result,
                 timeline: timeline
-                    )
+            )
 
             if let queue = queue ?? self.manager.completionQueue {
                 dispatch_async(queue) {
@@ -150,9 +150,9 @@ extension Request {
             } else {
                 completionHandler(response)
             }
-
-                }
             
+        }
+        
         return self
     }
 }
